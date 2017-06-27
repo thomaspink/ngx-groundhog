12332e2e0df4c6d6554672bc53e460d9a61faab3
Fabian Friedl <fabian.friedl@dynatrace.com>
2017-06-27 14:33:22 +0200

    groundhog module, button directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { GroundhogModule } from './groundhog/groundhog.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [GroundhogModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/groundhog/groundhog.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonDirective } from './button/button.directive';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ButtonDirective]
+})
+export class GroundhogModule { }
