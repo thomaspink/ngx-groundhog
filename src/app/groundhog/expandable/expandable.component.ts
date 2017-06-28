126c17a5bc306e5fa6d8262bc1eeaa3dbc275e6b
Fabian Friedl <fabian.friedl@dynatrace.com>
2017-06-28 11:49:22 +0200

    feat(expandable): added expandable

--- src/app/groundhog/expandable/expandable.component.ts
+++ src/app/groundhog/expandable/expandable.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'gh-expandable',
   templateUrl: './expandable.component.html',
-  styleUrls: ['./expandable.component.css']
+  styleUrls: ['./expandable.component.scss']
 })
 export class ExpandableComponent implements OnInit {
+  
+  @Input() title:string;
+  @Input() active:boolean = false;
+  @Input() trigger:string = 'left';
+  @Input() separated:boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    if(!this.title || this.title === '') throw new Error("Attribute 'title' is required");
+  }
+
+  onTrigger() {
+    this.active = !this.active;
   }
 
 }
