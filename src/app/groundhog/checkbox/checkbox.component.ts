f0c7d301b193bad2291549af44c320124ddaccdc
Freihofner, Katrin <Katrin.Freihofner@dynatrace.com>
2017-06-28 09:12:05 +0200

    started with init component

---
+++ src/app/groundhog/checkbox/checkbox.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gh-checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.css']
+})
+
+export class CheckboxComponent {
+  componentName: 'AppComponent'
+  constructor() {
+    console.log('My name is Katrin')
+  }
+}
--- src/app/groundhog/groundhog.module.ts
+++ src/app/groundhog/groundhog.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonDirective } from './button/button.directive';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ButtonDirective]
+  declarations: [ButtonDirective, CheckboxComponent]
 })
 export class GroundhogModule { }
