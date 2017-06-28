99ee7ee2227be39c1809a06c4324705c41274caf
Fabian Friedl <fabian.friedl@dynatrace.com>
2017-06-28 09:11:50 +0200

    added export and import

--- src/app/groundhog/groundhog.module.ts
+++ src/app/groundhog/groundhog.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonDirective } from './button/button.directive';
+import { ExpandableComponent } from './expandable/expandable.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ButtonDirective]
+  exports: [ButtonDirective, ExpandableComponent],
+  declarations: [ButtonDirective, ExpandableComponent]
 })
 export class GroundhogModule { }
