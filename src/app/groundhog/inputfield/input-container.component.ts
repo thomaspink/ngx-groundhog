c276c491e30d025f2a394f41f16428dcfb41ec13
Fabian Friedl <fabian.friedl@dynatrace.com>
2017-06-29 16:29:27 +0200

    test(input): more tests, ids

--- src/app/groundhog/inputfield/input-container.component.ts
+++ src/app/groundhog/inputfield/input-container.component.ts
@@ -37,14 +37,17 @@ export class InputContainerComponent implements AfterViewInit, AfterContentInit,
 
   @Input()
   get uid() { return this._uid; }
-  set uid(value: string) {this._uid = value }
+  set uid(value: string) {this._uid = value || `gh-input-container-${nextUniqueId++}` }
 
   @ContentChild(InputfieldDirective) _inputChild: InputfieldDirective;
   @ContentChild(LabelDirective) _labelChild: LabelDirective;
 
   @ContentChild(HintDirective) _hint: HintDirective;
 
-  constructor(public _elementRef: ElementRef, private _changeDetectorRef: ChangeDetectorRef) {}
+  constructor(public _elementRef: ElementRef, private _changeDetectorRef: ChangeDetectorRef) {
+    // Force setter to be called in case id was not specified.
+    this.uid = this.uid;
+  }
 
   ngAfterContentInit() {
     this._validateInputChild();
--- src/app/groundhog/inputfield/inputfield.directive.spec.ts
+++ src/app/groundhog/inputfield/inputfield.directive.spec.ts
@@ -14,6 +14,10 @@ describe('InputfieldDirective', () => {
       ],
       declarations: [
         InputContainerTestController,
+        InputContainerWithUidTestController,
+        InputContainerWithInputIdTestController,
+        InputContainerWithLabelTestController,
+        InputContainerHintTestController,
       ],
     });
 
@@ -26,17 +30,49 @@ describe('InputfieldDirective', () => {
     expect(fixture).toBeTruthy();
   });
 
-  it('should add id', () => {
+  it('should add id if there is no id set', () => {
     let fixture = TestBed.createComponent(InputContainerTestController);
     fixture.detectChanges();
     const inputElement: HTMLInputElement =
         fixture.debugElement.query(By.css('input')).nativeElement;
-    // const labelElement: HTMLInputElement =
-    //     fixture.debugElement.query(By.css('label')).nativeElement;
-
     expect(inputElement.id).toBeTruthy();
-    // expect(inputElement.id).toEqual(labelElement.getAttribute('for'))
   });
+
+  it('should take uid of container and apply it to id of input', () => {
+    let fixture = TestBed.createComponent(InputContainerWithUidTestController);
+    fixture.detectChanges();
+    const inputElement: HTMLInputElement =
+        fixture.debugElement.query(By.css('input')).nativeElement;
+    expect(inputElement.id).toBeTruthy();
+  });
+
+  it('should use existing id', () => {
+    let fixture = TestBed.createComponent(InputContainerWithInputIdTestController);
+    fixture.detectChanges();
+    const inputElement: HTMLInputElement =
+        fixture.debugElement.query(By.css('input')).nativeElement;
+    expect(inputElement.id).toEqual('1');
+  });
+
+  it('should have the inputs id as the label for prop', () => {
+    let fixture = TestBed.createComponent(InputContainerWithLabelTestController);
+    fixture.detectChanges();
+    const inputElement: HTMLInputElement =
+        fixture.debugElement.query(By.css('input')).nativeElement;
+    const labelElement: HTMLInputElement =
+        fixture.debugElement.query(By.css('label')).nativeElement;
+    expect(inputElement.id).toEqual(labelElement.getAttribute('for'));
+  });
+
+  it('should set the ariaDescribedby prop if there is a hint', () => {
+    let fixture = TestBed.createComponent(InputContainerHintTestController);
+    fixture.detectChanges();
+    const inputElement: HTMLInputElement =
+        fixture.debugElement.query(By.css('input')).nativeElement;
+    const hintElement: HTMLInputElement =
+        fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(inputElement.getAttribute('aria-describedby')).toEqual(hintElement.id);
+  })
 });
 
 @Component({
@@ -46,3 +82,37 @@ describe('InputfieldDirective', () => {
   </gh-input-container>`
 })
 class InputContainerTestController {}
+
+@Component({
+  template: `
+  <gh-input-container uid="1">
+    <input gh-inputfield type="text">
+  </gh-input-container>`
+})
+class InputContainerWithUidTestController {}
+
+@Component({
+  template: `
+  <gh-input-container>
+    <input gh-inputfield id="1" type="text">
+  </gh-input-container>`
+})
+class InputContainerWithInputIdTestController {}
+
+@Component({
+  template: `
+  <gh-input-container>
+    <label gh-label></label>
+    <input gh-inputfield type="text">
+  </gh-input-container>`
+})
+class InputContainerWithLabelTestController {}
+
+@Component({
+  template: `
+  <gh-input-container>
+    <input gh-inputfield type="text">
+    <p gh-hint></p>
+  </gh-input-container>`
+})
+class InputContainerHintTestController {}
